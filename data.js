85d62a985768925082488ac046d8156a769d44b1
zi <the2twentysixth6@gmail.com>
2025-01-28 13:26:04 +0800

    Created data.js - holds data to show

---
+++ data.js
@@ -0,0 +1,11 @@
+const profile1 = {
+     username: "@AkoSiDarna",
+     display_name: "DARNA",
+     bio: "", /*"Tech enthusiast and occasional gamer with a love for coding, creativity, " +
+          "and meaningful conversations. I enjoy exploring new ideas, diving into pop culture, and " +
+          "learning something new every day. Offline, you’ll find me experimenting in the kitchen or " +
+          "lost in a good book. Let’s share and grow together here!",*/
+     likes: 3,
+     posts: 2,
+     tags: ["#Movies", "#TVSeries", "#IndieFilms", "#SZA"]
+};
