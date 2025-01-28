8aa6186d56fad33bf504f822c08cad167078fdb8
Kelsey <gabriellemkelsey@gmail.com>
2025-01-28 19:52:54 +0800

    Home Page & Post Page - Separated scripts
    - Nested comments from post page
    - Action buttons from post page
    - Receiving post data from post page

---
+++ receivingPostData.js
@@ -0,0 +1,26 @@
+// Get content from localStorage and display it
+const contentData = JSON.parse(localStorage.getItem('postData'));
+
+if (contentData) {
+     document.getElementById('postTitle').innerText = `${contentData.postTitle}`;
+     document.getElementById('postText').innerText = `${contentData.postText}`;
+
+     if (contentData.postImage) {
+          document.getElementById('placeholderContent').innerHTML = `<img src="${contentData.postImage}" id="postImage">`;
+     }
+
+     // Display tags as individual <a> elements
+     if (contentData.postTags && contentData.postTags.length > 0) {
+          const tagsContainer = document.getElementById('tags');
+          contentData.postTags.forEach(tag => {
+               const tagElement = document.createElement('a');
+               tagElement.href = `${tag}`;  // Set href (you can change this to a proper URL if needed)
+               tagElement.innerText = `${tag}`;  // Display the tag with a '#' symbol
+               tagsContainer.appendChild(tagElement);  // Append each tag as an individual <a> tag
+          });
+     }
+
+} else {
+     document.getElementById('post').innerText = "Post Not Found";
+     document.getElementById('comments').innerText = " ";
+}
