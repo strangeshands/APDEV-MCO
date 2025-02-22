b2f731fb12bdd1b5de9d722cf3a6bda91ad69cb6
prantinin <francine_santos@dlsu.edu.ph>
2025-02-22 22:11:32 +0800

    Model - fixed users model

--- models/users.js
+++ models/users.js
@@ -5,19 +5,16 @@ const userSchema = new Schema({
     username: {
         type: String,
         required: true,
-        unique: true,
-        maxlength: [30, "Username must be at most 30 characters long."]
+        unique: true
     },
     displayname: {
         type: String,
-        required: true,
-        maxlength: [30, "Display name must be at most 30 characters long."]
+        required: true
     },
     email: {
         type: String,
         required: true,
-        unique: true,
-        match: [/.+\@.+\..+/, "Please enter a valid email address"] // Optional email validation
+        unique: true
     },
     phone: {
         type: String, // stored as string to retain leading zeroes
@@ -26,19 +23,12 @@ const userSchema = new Schema({
     },
     password: {
         type: String,
-        required: true,
-        validate: {
-            validator: function (value) {
-                return /^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/.test(value);
-            },
-            message: "Password must be at least 8 characters long and include at least 1 uppercase letter, 1 number, and 1 symbol."
-        }
+        required: true
     },
     bio: {
         type: String,
         required: true,
-        default: "Let's connect!",
-        maxlength: [100, "Bio must be at most 100 characters long."]
+        default: "Let's connect!"
     },
     profilepic: {
         type: String,      // image url is stored here
