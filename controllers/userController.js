80aab4cc1779c4072cecc1149aae453435724e96
zi <161949268+strangeshands@users.noreply.github.com>
2025-02-23 18:45:06 +0800

    Organized userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,39 @@
-/**
- *  feel free to make major changes; made this file as a tester only
- */
-
-/* ------------------- */
-
 const users = require('../models/users');
+const posts = require('../models/posts');
 
 /**
  *  > for testing only, will find for a specific hard coded user
- *  > TO UPDATE: find active user based on who logged in
+ * 
+ *  > TO DO: 
+ *      > find active user based on
+ *          > (1) the profile clicked
+ *          > (2) default is the user logged in
+ *      > likes count
  */
-const loadUserProfile = (req,res) => {
-    const activeUser = users.findOne({ name: "@AkoSiDarna" });
-    console.log(activeUser);
+const user = "@AkoSiDarna"
+const loadUserProfile = async (req,res) => {
+    // query profile
+    const profileSelected = await users.findOne({ username: user });
+    console.log(profileSelected);
 
-    res.render('profilePage', {
-        user: activeUser,
-    })
+    if (!profileSelected) {
+        return res.status(404).render("errorPage");
+    } else {
+        // query post count and likes count
+        const postCount = await posts.countDocuments({ author: user });
+        const likesCount = 0;
+
+        console.log(`Post Count: ${postCount}`);
+        console.log(`Likes Count: ${likesCount}`);
+
+        res.render('profilePage', {
+            profile: profileSelected,
+            postCount: postCount,
+            likesCount: likesCount
+        })
+    }
+};
+
+module.exports = { 
+    loadUserProfile 
 };
