91e455145fa8f3701be50e9367d69a320a4227c4
zi <161949268+strangeshands@users.noreply.github.com>
2025-02-24 12:09:37 +0800

    userController.js - added likes and bookmarks consts

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,9 +8,9 @@ const posts = require('../models/posts');
  *      > find active user based on
  *          > (1) the profile clicked
  *          > (2) default is the user logged in
- *      > likes count
  */
-const user = "@AkoSiDarna"
+const user = "@AkoSiDarna";
+
 const loadUserProfile = async (req,res) => {
     // query profile
     const profileSelected = await users.findOne({ username: user });
@@ -20,14 +20,32 @@ const loadUserProfile = async (req,res) => {
         return res.status(404).render("errorPage");
     } else {
         // query post count and likes count
-        const postCount = await posts.countDocuments({ author: user });
+        const postCount = await posts.countDocuments({ author: profileSelected });
         console.log(`Post Count: ${postCount}`);
+
+        /**
+         *  TO DO: query likes count properly
+         */
         const likesCount = 0;
         console.log(`Likes Count: ${likesCount}`);
 
         // query profile posts
-        const profilePosts = await posts.find({ author: user });
+        const profilePosts = await posts.find({ author: profileSelected }).populate('author');
         console.log(profilePosts);
+
+        /**
+         *  TO DO: query likes, replace null
+         */
+        // query profile likes
+        const profileLikes = null;
+        console.log(profileLikes);
+
+        /**
+         *  TO DO: query bookmars, replce null
+         */
+        // query profile bookmarks
+        const profileBookmarks = null;
+        console.log(profileBookmarks);
 
         // render profile page
         res.render('profilePage', {
@@ -35,7 +53,9 @@ const loadUserProfile = async (req,res) => {
             postCount: postCount,
             likesCount: likesCount,
 
-            profilePosts: profilePosts
+            profilePosts: profilePosts,
+            profileLikes: profileLikes,
+            profileBookmarks: profileBookmarks
         })
     }
 };
