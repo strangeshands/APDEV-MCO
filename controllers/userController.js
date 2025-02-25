261cfc4bbe32333eb2be15a843890e0bbdb02bc0
zi <161949268+strangeshands@users.noreply.github.com>
2025-02-25 15:36:51 +0800

    [MASS COMMIT] Adjusted userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const users = require('../models/users');
 const posts = require('../models/posts');
+const likes = require('../models/likes');
 
 /**
  *  > for testing only, will find for a specific hard coded user
@@ -9,57 +10,149 @@ const posts = require('../models/posts');
  *          > (1) the profile clicked
  *          > (2) default is the user logged in
  */
+//const user = "@jabeedabee";
 const user = "@AkoSiDarna";
 
-const loadUserProfile = async (req,res) => {
-    // query profile
-    const profileSelected = await users.findOne({ username: user });
-    console.log(profileSelected);
+// Initialize variables
+let profileDetails;
+let postCount;
+let likesCount;
 
-    if (!profileSelected) {
+const loadUserProfile = async(req,res) => {
+    let tabId = req.params.tabId;
+
+    // default is posts if tabId is null
+    if (!tabId) 
+        tabId = "posts"
+
+    // query profile
+    profileDetails = await users.findOne({ username: user });
+    console.log(profileDetails);
+
+    if (!profileDetails) {
         return res.status(404).render("errorPage");
     } else {
-        // query post count and likes count
-        const postCount = await posts.countDocuments({ author: profileSelected });
+        // query post count
+        postCount = await posts.countDocuments({ author: profileDetails });
         console.log(`Post Count: ${postCount}`);
 
-        /**
-         *  TO DO: query likes count properly
-         */
-        const likesCount = 0;
+        // query likes count
+        likesCount = await likes.countDocuments({ likedBy: profileDetails });
         console.log(`Likes Count: ${likesCount}`);
 
         // query profile posts
-        const profilePosts = await posts.find({ author: profileSelected }).populate('author');
+        const profilePosts = await posts
+            .find({ author: profileDetails })
+            .populate('author')
+            .populate({
+                path: 'parentPost',
+                populate: { path: 'author' }
+            })
+            .sort({ createdAt: -1 });
         console.log(profilePosts);
 
-        /**
-         *  TO DO: query likes, replace null
-         */
-        // query profile likes
-        const profileLikes = null;
-        console.log(profileLikes);
+        // query profile comments
+        const profileComments = await posts
+            .find({ 
+                author: profileDetails, 
+                parentPost: { $ne: null }
+            })
+            .populate('author')
+            .populate({
+                path: 'parentPost',
+                populate: { path: 'author' }
+            })
+            .sort({ createdAt: -1 });
 
-        /**
-         *  TO DO: query bookmars, replce null
-         */
         // query profile bookmarks
-        const profileBookmarks = null;
+        const bookmarksTemp = await users
+            .findOne({ username: user })
+            .populate({
+                path: 'bookmarks',
+                populate: [
+                    { path: 'author' },  
+                    { 
+                        path: 'parentPost', 
+                        populate: { path: 'author' } 
+                    }
+                ]
+            })
+            .select('bookmarks');
+        const profileBookmarks = bookmarksTemp.bookmarks.map(bookmark => bookmark);
         console.log(profileBookmarks);
 
-        // render profile page
-        res.render('profilePage', {
-            profileDetails: profileSelected,
-            postCount: postCount,
-            likesCount: likesCount,
+        // query profile likes
+        const likesTemp = await likes
+            .find({ likedBy: profileDetails })
+            .populate({
+                path: 'likedPost',
+                populate: [
+                    { path: 'author' },
+                    { 
+                        path: 'parentPost', 
+                        populate: { path: 'author' }  
+                    }
+                ]
+            })
+            .select('likedPost')
+        const profileLikes = likesTemp.map(like => like.likedPost);
+        console.log(profileLikes);
 
-            profilePosts: profilePosts,
-            profileLikes: profileLikes,
-            profileBookmarks: profileBookmarks
-        })
+        // query profile dislikes
+        const dislikesTemp = await users
+            .findOne({ username: user })
+            .populate({
+                path: 'dislikes',
+                populate: [
+                    { path: 'author' },  
+                    { 
+                        path: 'parentPost', 
+                        populate: { path: 'author' } 
+                    }
+                ]
+            })
+            .select('dislikes');
+        const profileDislikes = dislikesTemp.dislikes.map(dislike => dislike);
+        console.log(profileDislikes);
+
+        res.render("profilePage", {
+            activeTab: tabId,
+            
+            profileDetails,
+            likesCount,
+            postCount,
+
+            profilePosts,
+            profileComments,
+            profileBookmarks,
+            profileLikes,
+            profileDislikes
+        });
     }
 };
 
+const updateBookmark = async(req,res) => {
+    const { postId, action } = req.body;
+    profileDetails = await users.findOne({ username: user });
+
+    if (!profileDetails) {
+        return res.status(404).json({ success: false, message: 'User not found' });
+    }
+
+    if (action === 'add') {
+        if (!profileDetails.bookmarks.includes(postId)) {
+            profileDetails.bookmarks.splice(0, 0, postId);
+        }
+    } else if (action === 'remove') {
+        await users.updateOne(
+            { _id: profileDetails._id },
+            { $pull: { bookmarks: postId } }
+        );
+    }
+    await profileDetails.save();
+}; 
+
 module.exports = { 
-    loadUserProfile 
+    loadUserProfile, 
+    updateBookmark
 };
