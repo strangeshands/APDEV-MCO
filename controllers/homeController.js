5f30b667e136990f0dcf66d642b7f8dec4893c2f
haniellejermayn <chua.haniellejermayn@gmail.com>
2025-03-02 10:17:06 +0800

    Updated homeController to fetch navbar posts and comments

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,13 +1,65 @@
 const Post = require('../models/posts');
+const User = require('../models/users');
 const moment = require('moment');   // For time display
 
-// Render the Home Page
-/*
-router.get('/', (req, res) => {
-    res.render('homePage', { title: "Connectify - Home" });
-});
-*/
+// ---- [START] TEST for Home with NavBar ---- //
 
+// Default user ID (will replace with actual logged-in user after login implementation)
+const tempUserId = "67b9fd7ab7db71f6ae3b21d4";
+
+const homePage = async (req, res) => {
+    try {
+        // Query the default user
+        const userPromise = User.findById(tempUserId).exec();
+
+        // All posts for the timeline
+        const allPostsPromise = Post.find()
+            .sort({ createdAt: -1 })
+            .populate('author')
+            .exec();
+        
+        // Default (logged-in) user's posts for navbar
+        const userPostsPromise = Post.find({ author: tempUserId })
+            .sort({ createdAt: -1 })
+            .populate('author')
+            .exec()
+
+        // Default (logged-in) user's comments for navbar
+        const userCommentsPromse = Post.find({ parentPos: { $ne: null }, author: tempUserId})
+            .sort({ createdAt: -1 })
+            .populate('author')
+            .exec()
+
+        // Run all queries
+        const [user, allPosts, userPosts, userComments] = await Promise.all([
+            userPromise,
+            allPostsPromise,
+            userPostsPromise,
+            userCommentsPromise
+        ]);
+
+        // Check if user exists
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+
+        // Render homepage
+        res.render('homeTemp', {
+            title: 'Home',
+            post: allPosts,
+            user,
+            userPosts,
+            userComments
+        });
+    } catch(err) {
+        consoler.error(err);
+        res.status(500).send("Internal Server Error");
+    }
+};
+
+// ---- [END] TEST for Home with NavBar ---- //
+
+/*
 // Renders the Home Page
 const homePage = (req, res) => {
     Post.find().sort({ createdAt: -1 }) // sorts to show latest entry
@@ -20,5 +72,6 @@ const homePage = (req, res) => {
             console.log(err)
         });
 };
+*/
 
 module.exports = { homePage };
