d71286f15d32604edeeea160ee7769d74a7e643f
haniellejermayn <chua.haniellejermayn@gmail.com>
2025-03-07 22:40:06 +0800

    updated postRoutes for reply

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -18,8 +18,12 @@ router.delete('/:id', postController.post_delete);
 // draft
 router.use("/delete/:postId", postController.deletePost);
 
-router.use("/edit/:postId", postController.editPostLoad);
+router.get("/edit/:postId", postController.editPostLoad);
 
 router.post("/edit-save/:postId", postController.editPostSave);
+
+// Reply routes
+router.get("/reply/:id", postController.reply_create_get);
+router.post("/reply/:id", postController.reply_create_post);
 
 module.exports = router;
