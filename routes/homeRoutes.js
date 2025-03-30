79e29b9a0500c83c384ab5b03329d4b8519d87e0
zi <the2twentysixth6@gmail.com>
2025-03-30 15:35:41 +0800

    cleaned up homeRoutes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeController');
+
 const Post = require('../models/posts');
 const User = require('../models/users');
 
-
 // GET
 router.get('/', homeController.homePage);
-
 router.get('/search/suggestions', homeController.search);
 
 module.exports = router;
-
